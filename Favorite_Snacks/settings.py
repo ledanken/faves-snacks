ecbdbb83b6b1542916f98fb5409d5acd1ab02234
ledanken <ledna.ankenbauer@yahoo.com>
2024-04-03 15:28:52 +0000

    Connect the code of Faves-Snacks to Postgresol Database.

--- Favorite_Snacks/settings.py
+++ Favorite_Snacks/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!k@c2px9t!a1y#9ad=agc0-z6*w8j(5masw)yo7wo=w-_2110h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ledanken-faves-snacks-fpqipumh8a.us1.codeanyapp.com',
                 '.herokuapp.com']
@@ -75,13 +79,17 @@ WSGI_APPLICATION = 'Favorite_Snacks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+DATABASES = {
+    'default':
+    dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
