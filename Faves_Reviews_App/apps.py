308fdc8dbc7ebcda039ac821f45cf1296b095e78
ledanken <ledna.ankenbauer@yahoo.com>
2024-04-03 10:33:31 +0000

    Create the App Fave_Reviews_App. Modify the views, the urls and the setings for it.

---
+++ Faves_Reviews_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FavesReviewsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Faves_Reviews_App'
--- Favorite_Snacks/settings.py
+++ Favorite_Snacks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Faves_Reviews_App',
 ]
 
 MIDDLEWARE = [
